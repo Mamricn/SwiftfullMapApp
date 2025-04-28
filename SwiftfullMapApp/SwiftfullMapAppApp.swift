5d3bdb8f5731d4b36fe3d53bb4ddc46bfe3432da
Mamricn <marcin.turek.27@wp.pl>
2025-04-28 11:58:53 +0100

    Initial Commit

---
+++ SwiftfullMapApp/SwiftfullMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfullMapAppApp.swift
+//  SwiftfullMapApp
+//
+//  Created by Marcin Turek on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfullMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
