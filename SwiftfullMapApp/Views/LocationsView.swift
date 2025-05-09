fefe1286f14515081065cb5f33f3ebc62de0bd4b
Mamricn <marcin.turek.27@wp.pl>
2025-05-09 18:40:07 +0100

    [Complition] complition of App

--- SwiftfullMapApp/Views/LocationsView.swift
+++ SwiftfullMapApp/Views/LocationsView.swift
@@ -12,19 +12,23 @@ import MapKit
 struct LocationsView: View {
     @EnvironmentObject private var vm: LocationsViewModel
     
+    let maxWidthForIpad: CGFloat = 400
+    
 
     var body: some View {
         ZStack{
-            // vm odwoluje sie do foldera LocationsViewModel
-            //a wszystko po vs. to sa osobne funckcje 
-            Map(position: $vm.mapRegion)
+            mapLayer
             
             VStack(spacing:0){
                header
                     .padding()
-                
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
+                locationsPreviewStack
             }
+        }
+        .sheet(item: $vm.sheetLocation, onDismiss: nil) { location in
+            LocationDetailView(location: location)
         }
     }
 }
@@ -45,6 +49,7 @@ extension LocationsView {
                     .foregroundStyle(.black)
                     .frame(height: 55)
                     .frame(maxWidth: .infinity)
+                    .animation(.none, value: vm.mapLocation)
                     .overlay(alignment: .leading) {
                         Image(systemName: "arrow.down")
                             .font(.headline)
@@ -66,4 +71,45 @@ extension LocationsView {
         .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
         
     }
+    
+    
+    private var mapLayer: some View {
+        // vm odwoluje sie do foldera LocationsViewModel
+        //a wszystko po vs. to sa osobne funckcje
+        Map(position: $vm.mapRegion){
+            ForEach(vm.locations){ location in
+                //                Marker(location.name, coordinate: location.coordinates)
+                Annotation(location.name,
+                           coordinate: location.coordinates){
+                    LocationMapAnnotationView()
+                        .scaleEffect(vm.mapLocation == location ? 1.0 : 0.7)
+                }
+            }
+            
+        }
+    }
+    
+    private var locationsPreviewStack: some View{
+        ZStack{
+            // tu sie loop robi z calej array locations vm znaczy ze to jest z innej storny pobierane
+            ForEach(vm.locations){ location in
+                //jeszeli aktualna lokalizacja mapy jest rowna jedna z tych z array to wyswietli sie tylko jedno zdjecie
+                if vm.mapLocation == location{
+                    //tu jest reference do stworzonej juz strony i ona ma zostac
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
+                        .transition(.asymmetric(insertion: .move(edge: .trailing),
+                                                removal: .move(edge: .leading)))
+                }
+            }                }
+    }
+    
+    
+    
+    
+    
+    
 }
